2b4650998ec2f555108e0b0d88d279cb7e67bc7a
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2023-04-23 16:29:43 +0300

    Quant.cs first release
    First release of Quant.cs!

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Quant.Cs {
+    class QuantCore
+    {
+        public List<IQuantCommand> commands;
+        public string pathFormat = "\\";
+        public QuantCore(List<IQuantCommand> commands)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            { // Check OS
+                pathFormat = "/";
+                Environment.CurrentDirectory = "/home/";
+            }
+            else
+            {
+                Environment.CurrentDirectory = $"C:\\Users\\{Environment.UserName}\\";
+            }
+            this.commands = commands;
+        }
+
+        public void Launch()
+        {
+            Console.WriteLine("Launched!");
+            while (true)
+            {
+                Console.Write($"{Environment.UserName}@{Environment.CurrentDirectory} > ");
+                string argument = Console.ReadLine();
+                foreach (var item in commands)
+                {
+                    if (item.name.ToLower() == argument.Split()[0])
+                    {
+                        item.Execute(argument.Replace($"{argument.Split()[0]} ", "")); // If command name equals first word of input, execute command
+                    }
+                }
+            }
+        }
+
+
+    }
+    interface IQuantCommand
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public void Execute(params string[] arg)
+        {
+
+        }
+    }
+}
